04007aac01d0ece61508032b0ab2f595ae180dad
Yehor <muregor.egor@gmail.com>
2024-04-06 17:42:07 +0300

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/auth/*", "/signin", "/signout"], 
+  publicRoutes: ["/", "/api/auth/*"], 
 });
 
 export const config = {
